612a35c27ccdc8dca950852c6b5a27ff2ee14bda
penguinasian <zhangqiangtianchen@gmail.com>
2023-01-17 08:54:02 -0800

    added skills section and css styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,27 @@ function Home() {
         </div>
       </div>
       <div className="skills">
-
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, VueJS, HTML, CSS, Bootstrap, TaiwindCSS, NPM, Android Native
+            </span>
+          </li>
+          <li className="item">
+            <h2> Back-End </h2>
+            <span>
+              NodeJS, ExpressJS, Postgresql, MongoDB, DynamoDB, AWS S3, SQL
+            </span>
+          </li>
+          <li className="item">
+            <h2> Languages </h2>
+            <span>
+              Python, Javascript, Java, C, TypeScript
+            </span>
+          </li>
+        </ol>
       </div>
     </div>
   )
